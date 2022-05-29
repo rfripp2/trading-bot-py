71a1fad5518e7483ff3bcfc08fa0aa2b26725e7d
rfripp2 <juangonzalez465@gmail.com>
2022-05-28 22:15:50 -0300

    refactored to Class

--- Classes.py
+++ Classes.py
@@ -1,86 +1,96 @@
+from more_itertools import last
 import pandas as pd
 import numpy as np
-import time
-from urllib.parse import urlencode, urljoin
 from binance.websocket.futures.websocket_client import FuturesWebsocketClient as Client
 from binance.futures import Futures
 from dotenv import load_dotenv
 import os
 from binance.futures import Futures
-from binance.error import ClientError
-from binance.lib.utils import config_logging
-import logging
 from binance_orders import *
-load_dotenv()
+
+config_logging(logging, logging.DEBUG)
 
 
-class Bot():
-    def __init__(self, quantity):
-        self.quantity = quantity
-        self.config = {
-            "position_running": False
-        }
-        self.clientFutures = Futures(key=os.getenv("API_KEY"),
-                                     secret=os.getenv("SECRET_KEY"))
-
-    def bullish_engulfing_handler(self, message):
-        first_two_candels = self.clientFutures.klines(
+class Bot:
+    def __init__(self, api_key, secret_key, balance, timeframe):
+        self.balance = balance
+        self.config = {"position_running": False}
+        self.clientFutures = Futures(api_key, secret_key)
+        self.timeframe = timeframe
+        self.first_two_candels = self.clientFutures.klines(
             "BTCUSDT", "1m", limit=2, contractType="perpetual"
         )
-        order_pairs = []
-        closed_candels = [{"o": first_two_candels[0]
-                           [1], "c": first_two_candels[0][4], "color": "unknwown"}]
-        if (len(order_pairs) > 0):
-            first_pair = order_pairs[0]
-            if_filled_cancell_other(
-                first_pair['limit_sell_order_id'], first_pair['stop_market_order_id'])
+        self.order_pair = {}
+        self.closed_candels = [
+            {
+                "o": self.first_two_candels[0][1],
+                "c": self.first_two_candels[0][4],
+                "color": "unknwown",
+            }
+        ]
+
+        self.counter = 0
+
+    def bullish_engulfing_handler(self, message):
+
         kline = message.get("k")
         if kline != None:
             if kline["x"]:
+                if bool(self.order_pair):
+                    if_filled_cancell_other(
+                        self.order_pair["limit_sell_order_id"],
+                        self.order_pair["stop_market_order_id"],
+                        self.clientFutures,
+                        self,
+                    )
                 print("KLINE:", kline)
-                if kline["c"] > closed_candels[len(closed_candels) - 1]["c"]:
+                if float(kline["c"]) > float(
+                    self.closed_candels[len(self.closed_candels) - 1]["c"]
+                ):
                     kline["color"] = "green"
                 else:
                     kline["color"] = "red"
-                closed_candels.append(kline)
-                last_candle = closed_candels[-1]
-                penultimate_candle = closed_candels[-2]
+                self.closed_candels.append(kline)
+                last_candle = self.closed_candels[-1]
+                penultimate_candle = self.closed_candels[-2]
+
                 if (
-                    last_candle["c"]
-                    > penultimate_candle["o"]
+                    float(last_candle["c"]) > float(penultimate_candle["o"])
                     and penultimate_candle["color"] == "red"
-                    and self.config['position_running'] == False
+                    and self.config["position_running"] == False
                 ):
                     print("its bullish!")
-                    market_buy(self.quantity, self.clientFutures)
-                    limit_sell_price = get_limit_sell_price_from_price(
-                        kline['c'], 1)
-                    limitSell(self.quantity, limit_sell_price,
-                              self.clientFutures)
-                    all_orders = get_all_orders()
-                    limit_sell_order_id = all_orders[len(
-                        all_orders)-1]['orderId']
+                    ammount_to_buy = round(self.balance / float(kline["c"]), 3)
+                    print("AMMOUNT TO BUY:", ammount_to_buy)
+                    market_buy(ammount_to_buy, self.clientFutures)
+                    sell_stop_market_price = round(
+                        float(kline["l"]) - atr(self.clientFutures, self.timeframe), 2
+                    )
+                    stop_market(
+                        sell_stop_market_price, ammount_to_buy, self.clientFutures
+                    )
 
-                    sell_stop_market_price = get_stop_market_price_from_price(
-                        kline['c'], 1)
-                    stop_market(0.001, sell_stop_market_price,
-                                self.clientFutures)
-                    all_orders = get_all_orders()
-                    stop_market_order_id = all_orders[len(
-                        all_orders)-1]['orderId']
-                    order_pairs.append({"limit_sell_order_id": limit_sell_order_id,
-                                        "stop_market_order_id": stop_market_order_id})
-                    self.config['position_running'] = True
-                print("order pairs", order_pairs)
+                    limit_sell_price = round(
+                        float(kline["c"]) - sell_stop_market_price + float(kline["c"]),
+                        1,
+                    )
 
-    def run(self):
+                    limit_sell(limit_sell_price, ammount_to_buy, self.clientFutures)
+                    all_orders = get_all_orders(self.clientFutures)
+                    limit_sell_order_id = all_orders[len(all_orders) - 2]["orderId"]
+                    stop_market_order_id = all_orders[len(all_orders) - 1]["orderId"]
+                    self.order_pair["limit_sell_order_id"] = limit_sell_order_id
+                    self.order_pair["stop_market_order_id"] = stop_market_order_id
+                    self.config["position_running"] = True
+
+    def run_bullish_engulfing(self):
         my_client = Client()
         my_client.start()
         my_client.continuous_kline(
             pair="btcusdt",
             id=1,
             contractType="perpetual",
-            interval="1m",
+            interval=self.timeframe,
             callback=self.bullish_engulfing_handler,
         )
 
@@ -92,26 +102,34 @@ class Bot():
 
     def get_sma(self, window):
         candles = self.clientFutures.klines(
-            "BTCUSDT", "1m", limit=50, contractType="perpetual"
+            "BTCUSDT", self.timeframe, limit=window, contractType="perpetual"
         )
         pandas_array = np.array(candles)
-        df = pd.DataFrame(pandas_array, columns=['Open Time',
-                                                 'Open',
-                                                 'High',
-                                                 'Low',
-                                                 'Close',
-                                                 'Volume',
-                                                 'Close time',
-                                                 'Quote asset volume',
-                                                 'Number of trades',
-                                                 'Taker buy base asset volume',
-                                                 'Taker buy quote asset volume',
-                                                 'Ignore'])
-        df[f"SMA_{window}"] = df['Close'].rolling(window=int(window)).mean()
+        df = pd.DataFrame(
+            pandas_array,
+            columns=[
+                "Open Time",
+                "Open",
+                "High",
+                "Low",
+                "Close",
+                "Volume",
+                "Close time",
+                "Quote asset volume",
+                "Number of trades",
+                "Taker buy base asset volume",
+                "Taker buy quote asset volume",
+                "Ignore",
+            ],
+        )
+        df[f"SMA_{window}"] = df["Close"].rolling(window=int(window)).mean()
         last_sma = df.iloc[-1][f"SMA_{window}"]
-        print(str(last_sma))
-        return str(last_sma)
+        print(last_sma)
+        return last_sma
 
 
-bot = Bot(0.001)
-bot.run()
+# market_buy(0.002, bot.clientFutures)
+# bot.run()
+# market_buy(0.0029226093055880293, bot.clientFutures)
+# stop_market(0.001, 28200.000, bot.clientFutures)
+# limitSell(0.003, 29100.2, bot.clientFutures)
--- binance_orders.py
+++ binance_orders.py
@@ -1,19 +1,16 @@
-from concurrent.futures import process
 import time
-from binance.websocket.futures.websocket_client import FuturesWebsocketClient as Client
-from binance.futures import Futures
-from dotenv import load_dotenv
 import os
-from binance.futures import Futures
 from binance.error import ClientError
-from binance.lib.utils import config_logging
 import logging
 from urllib.parse import urlencode, urljoin
+from more_itertools import quantify
 import requests
 import hmac
 import hashlib
 import pandas as pd
 import numpy as np
+import talib
+from binance.lib.utils import config_logging
 
 
 def market_buy(quantity, client):
@@ -33,15 +30,15 @@ def market_buy(quantity, client):
         )
 
 
-def limitSell(quantity, price, client):
+def limit_sell(price, quantity, client):
     try:
         response = client.new_order(
             symbol="BTCUSDT",
             side="SELL",
             type="LIMIT",
-            quantity=quantity,
             timeInForce="GTC",
             price=price,
+            quantity=quantity,
         )
         logging.info(response)
     except ClientError as error:
@@ -52,7 +49,7 @@ def limitSell(quantity, price, client):
         )
 
 
-def stop_market(quantity, stopPrice, client):
+def stop_market(stopPrice, quantity, client):
     try:
         response = client.new_order(
             symbol="BTCUSDT",
@@ -60,7 +57,7 @@ def stop_market(quantity, stopPrice, client):
             type="STOP_MARKET",
             quantity=quantity,
             timeInForce="GTC",
-            stopPrice=stopPrice
+            stopPrice=stopPrice,
         )
         logging.info(response)
     except ClientError as error:
@@ -73,8 +70,7 @@ def stop_market(quantity, stopPrice, client):
 
 def get_all_orders(client):
     try:
-        response = client.get_all_orders(
-            symbol="BTCUSDT", recvWindow=2000)
+        response = client.get_all_orders(symbol="BTCUSDT", limit=2, recvWindow=2000)
         return response
     except ClientError as error:
         logging.error(
@@ -86,8 +82,7 @@ def get_all_orders(client):
 
 def get_open_order(id, client):
     try:
-        client.get_open_orders(
-            symbol="BTCUSDT", orderId=id, recvWindow=2000)
+        client.get_open_orders(symbol="BTCUSDT", orderId=id, recvWindow=2000)
     # logging.info(response)
 
     except ClientError as error:
@@ -98,11 +93,11 @@ def get_open_order(id, client):
         )
 
 
-def get_order(id):
+def get_order(id, client):
     found = False
-    all_orders = get_all_orders()
+    all_orders = get_all_orders(client)
     for order in all_orders:
-        if order['orderId'] == id:
+        if order["orderId"] == id:
             found = True
             return order
     if not found:
@@ -111,32 +106,25 @@ def get_order(id):
 
 
 def cancell_order(id):
-    PATH = '/fapi/v1/order'
-    BASE_URL = 'https://fapi.binance.com'
+    PATH = "/fapi/v1/order"
+    BASE_URL = "https://fapi.binance.com"
     timestamp = int(time.time() * 1000)
     secret_key = os.getenv("SECRET_KEY")
     api_key = os.getenv("API_KEY")
-    headers = {
-        'X-MBX-APIKEY': api_key
-    }
+    headers = {"X-MBX-APIKEY": api_key}
     try:
-        params = {
-            "symbol": "BTCUSDT",
-            "orderId": id,
-            'timestamp': timestamp
-        }
+        params = {"symbol": "BTCUSDT", "orderId": id, "timestamp": timestamp}
         query_string = urlencode(params)
-        params['signature'] = hmac.new(secret_key.encode(
-            'utf-8'), query_string.encode('utf-8'), hashlib.sha256).hexdigest()
+        params["signature"] = hmac.new(
+            secret_key.encode("utf-8"), query_string.encode("utf-8"), hashlib.sha256
+        ).hexdigest()
         url = urljoin(BASE_URL, PATH)
         r = requests.delete(url, headers=headers, params=params)
-        logging.info(r.json())
     except ClientError as error:
         logging.error(
             "Found error. status: {}, error code: {}, error message: {}".format(
                 error.status_code, error.error_code, error.error_message
             )
-
         )
 
 
@@ -150,33 +138,51 @@ def get_stop_market_price_from_price(price, percentage):
     return int(stop_sell_market_price)
 
 
-def if_filled_cancell_other(sell_limit_id, sell_stop_market_id):
-    sell_limit_order = get_order(sell_limit_id)
-    sell_stop_market_order = get_order(sell_stop_market_id)
-    if sell_limit_order['status'] == 'FILLED':
+def if_filled_cancell_other(sell_limit_id, sell_stop_market_id, client, self):
+
+    sell_limit_order = get_order(sell_limit_id, client)
+    sell_stop_market_order = get_order(sell_stop_market_id, client)
+
+    if sell_limit_order["status"] == "FILLED":
         cancell_order(sell_stop_market_id)
-    if sell_stop_market_order['status'] == "FILLED":
+        self.config["position_running"] = False
+        self.order_pair = {}
+
+    if sell_stop_market_order["status"] == "FILLED":
         cancell_order(sell_limit_id)
+        self.config["position_running"] = False
+        self.order_pair = {}
 
 
-def get_sma(window, client):
+def get_sma(window, client, timeframe):
     candles = client.klines(
-        "BTCUSDT", "1m", limit=50, contractType="perpetual"
+        "BTCUSDT", timeframe, limit=window, contractType="perpetual"
     )
     pandas_array = np.array(candles)
-    df = pd.DataFrame(pandas_array, columns=['Open Time',
-                                             'Open',
-                                             'High',
-                                             'Low',
-                                             'Close',
-                                             'Volume',
-                                             'Close time',
-                                             'Quote asset volume',
-                                             'Number of trades',
-                                             'Taker buy base asset volume',
-                                             'Taker buy quote asset volume',
-                                             'Ignore'])
-    df[f"SMA_{window}"] = df['Close'].rolling(window=int(window)).mean()
+    df = pd.DataFrame(
+        pandas_array,
+        columns=[
+            "Open Time",
+            "Open",
+            "High",
+            "Low",
+            "Close",
+            "Volume",
+            "Close time",
+            "Quote asset volume",
+            "Number of trades",
+            "Taker buy base asset volume",
+            "Taker buy quote asset volume",
+            "Ignore",
+        ],
+    )
+    df[f"SMA_{window}"] = df["Close"].rolling(window=int(window)).mean()
     last_sma = df.iloc[-1][f"SMA_{window}"]
-    print(str(last_sma))
-    return str(last_sma)
+    return last_sma
+
+
+def atr(client, timeframe):
+    candles = client.klines("BTCUSDT", timeframe, limit=15, contractType="perpetual")
+    df = pd.DataFrame(candles)
+    df["atr"] = talib.ATR(df[2], df[3], df[4], timeperiod=14)
+    return float(df.iloc[-1]["atr"])
